eda00d903be5fd2b4040be937c758516e1666b32
ostrokach <ostrokach@gmail.com>
2020-02-18 13:54:56 -0500

    Rename ExampleView to MSAView

--- src/plugin.ts
+++ src/plugin.ts
@@ -13,7 +13,7 @@ import {
   IJupyterWidgetRegistry
  } from '@jupyter-widgets/base';
 
-import * as widgetExports from './widget';
+import * as widgetExports from './widget_msa';
 
 import {
   MODULE_NAME, MODULE_VERSION
--- tests/src/index.spec.ts
+++ tests/src/index.spec.ts
@@ -12,24 +12,24 @@ import {
 } from './utils.spec';
 
 import {
-  ExampleModel, ExampleView
+  MSAModel, MSAView
 } from '../../src/'
 
 
 describe('Example', () => {
 
-  describe('ExampleModel', () => {
+  describe('MSAModel', () => {
 
     it('should be createable', () => {
-      let model = createTestModel(ExampleModel);
-      expect(model).to.be.an(ExampleModel);
+      let model = createTestModel(MSAModel);
+      expect(model).to.be.an(MSAModel);
       expect(model.get('value')).to.be('Hello World');
     });
 
     it('should be createable with a value', () => {
       let state = { value: 'Foo Bar!' }
-      let model = createTestModel(ExampleModel, state);
-      expect(model).to.be.an(ExampleModel);
+      let model = createTestModel(MSAModel, state);
+      expect(model).to.be.an(MSAModel);
       expect(model.get('value')).to.be('Foo Bar!');
     });
 
